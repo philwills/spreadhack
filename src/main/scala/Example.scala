f38b5c06af472652baf8a15094bc1ae593132d56
Philip Wills <philip.wills@guardian.co.uk>
2011-10-21 12:56:51 +0100

    Something printing sheetsish

--- src/main/scala/Example.scala
+++ src/main/scala/Example.scala
@@ -6,8 +6,10 @@ import unfiltered.Cookie
 
 import org.clapper.avsl.Logger
 import com.google.gdata.client.docs.DocsService
-import com.google.gdata.data.spreadsheet.SpreadsheetFeed
 import collection.JavaConversions._
+import com.google.gdata.data.spreadsheet._
+import com.google.gdata.data.TextContent
+import xml.{Elem, XML}
 
 /** unfiltered plan */
 class App extends unfiltered.filter.Plan {
@@ -34,7 +36,62 @@ class App extends unfiltered.filter.Plan {
       val service = new DocsService("open newslist hack")
       service.setOAuthCredentials(docs.DocAuth.params(cookies), docs.DocAuth.signer)
       val feed = service.getFeed(metafeedUrl, classOf[SpreadsheetFeed])
-      Ok ~> Html(<ul> { feed.getEntries map { entry => <li>{ entry.getTitle.getPlainText} </li> } } </ul>)
+      Ok ~> Html(<ul> { feed.getEntries map { entry => <li>
+        <a href={ "/ss/" + entry.getKey }>{ entry.getTitle.getPlainText}</a>
+      </li> } } </ul>)
+    }
+    case GET(Path(Seg("ss" :: key :: Nil)) & Cookies(cookies)) => {
+      val service = new DocsService("open newslist hack")
+      service.setOAuthCredentials(docs.DocAuth.params(cookies), docs.DocAuth.signer)
+      val metafeedUrl = new java.net.URL("https://spreadsheets.google.com/feeds/worksheets/%s/private/full" format (key))
+      val feed = service.getFeed(metafeedUrl, classOf[WorksheetFeed])
+      Ok ~> Html(<ul> { feed.getEntries map { entry =>
+        <li>
+          { entry.getTitle.getPlainText }
+          { renderSheet(entry, service) }
+        </li> } }
+      </ul>)
+    }
+  }
+  def renderSheet(sheet: WorksheetEntry, service: DocsService) = {
+    val listFeedUrl = sheet.getCellFeedUrl
+    val feed = service.getFeed(listFeedUrl, classOf[CellFeed])
+//    println((feed.getEntries filter {getCol(_) < 4}).size)
+    val interestingCols = feed.getEntries filter {col(_) < 4}
+    val rows = interestingCols.groupBy(row)
+    for (rowNum <- rows.keys.toSeq.sorted) {
+
+    }
+
+    <ul> { (feed.getEntries filter {x => (col(x) < 4) && (row(x) > 2)}) map  { entry =>
+      <li> { entry.getTitle.getPlainText }
+        </li>
+      <li>{entry.getXmlBlob.getBlob}
+        </li><li>
+        { linkFrom(inputValue(entry)) } </li>
+      <li> {XML.loadString(entry.getXmlBlob.getBlob).text} </li>
+    } } </ul>
+//    for (ListEntry entry : feed.getEntries()) {
+//    System.out.println(entry.getTitle().getPlainText());
+//    for (String tag : entry.getCustomElements().getTags()) {
+//      System.out.println("  <gsx:" + tag + ">" +
+//        entry.getCustomElements().getValue(tag) + "</gsx:" + tag + ">");
+//    }
+  }
+  def col(x: CellEntry) = attributeAsInt(x, "col")
+  def row(x: CellEntry) = attributeAsInt(x, "row")
+  def attributeAsInt(x: CellEntry, attributeName: String) = {
+//      println(XML.loadString(x.getXmlBlob.getBlob).attribute("col").get.head.text.toInt)
+    (XML.loadString(x.getXmlBlob.getBlob).attribute(attributeName)).get.head.text.toInt
+  }
+
+  def inputValue(x: CellEntry): String = XML.loadString(x.getXmlBlob.getBlob).attribute("inputValue") map (_.head.text) getOrElse("")
+  def linkFrom(hyperlinkText: String) = {
+    println(hyperlinkText)
+    val hyperLink = """=hyperlink\("(.*?)"; "(.*?)"\)""".r
+    hyperlinkText match {
+      case hyperLink(url, text) => Some((url, text))
+      case _ => None
     }
   }
 }
